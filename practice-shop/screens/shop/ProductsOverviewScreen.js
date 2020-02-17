71c6a79f5267eb4cafe57fd29e65111a9999c50a
Rafał Zubala <rzubala@gmail.com>
2020-02-17 21:55:52 +0100

    initial components

---
+++ practice-shop/screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { FlatList, View, Text, StyleSheet } from 'react-native'
+
+const ProductsOverviewScreen = props => {
+    return <FlatList />
+}
+
+const style = StyleSheet.create({
+
+})
+
+export default ProductsOverviewScreen;
