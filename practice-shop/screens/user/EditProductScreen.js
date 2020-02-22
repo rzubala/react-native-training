9cacc9f2fbe0405753b733a3824039ef1d162dfd
Rafał Zubala <rzubala@gmail.com>
2020-02-22 20:07:43 +0100

    use reducer

--- practice-shop/screens/user/EditProductScreen.js
+++ practice-shop/screens/user/EditProductScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react'
+import React, { useEffect, useCallback, useReducer } from 'react'
 import { View, Text, ScrollView, StyleSheet, TextInput, Platform, Alert } from 'react-native'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 import { useSelector, useDispatch } from 'react-redux'
@@ -7,51 +7,102 @@ import Colors from '../../constants/Colors'
 import HeaderButton from '../../components/UI/HeaderButton'
 import * as ProductActions from '../../store/actions/products'
 
+const FORM_INPUT_UPDATE = 'UPDATE'
+
+const formReducer = (state, action) => {    
+    if (action.type === FORM_INPUT_UPDATE) {
+        const updatedValues = {
+            ...state.inputValues,
+            [action.input]: action.value
+        }
+        const updatedValidaties = {
+            ...state.inputValidities,
+            [action.input]: action.isValid
+        }
+        let updatedFormIsValid = true
+        for (const key in updatedValidaties) {
+            updatedFormIsValid = updatedFormIsValid && updatedValidaties[key]
+        }
+        return {
+            formIsValid: updatedFormIsValid,
+            inputValues: updatedValues,
+            inputValidities: updatedValidaties
+        }        
+    }
+    return state
+}
+
 const EditProductsScreen = props => {
     const prodId = props.navigation.getParam('productId')
     const editedProduct = useSelector(state => state.products.userProducts.find(prod => prod.id === prodId))
 
-    const [title, setTitle] = useState(editedProduct ? editedProduct.title : '')
-    const [imageUrl, setImageUrl] = useState(editedProduct ? editedProduct.imageUrl : '')
-    const [price, setPrice] = useState('')
-    const [description, setDescription] = useState(editedProduct ? editedProduct.description : '')
-
     const dispatch = useDispatch()
 
+    const [formState, dispatchFormState] = useReducer(formReducer, {
+        inputValues: {
+            title: editedProduct ? editedProduct.title : '',
+            imageUrl: editedProduct ? editedProduct.imageUrl : '',
+            description: editedProduct ? editedProduct.description : '',
+            price: ''
+        },
+        inputValidities: {
+            title: editedProduct ? true : false,
+            imageUrl: editedProduct ? true : false,
+            description: editedProduct ? true : false,
+            price: editedProduct ? true : false,
+        },
+        formIsValid: editedProduct ? true : false,
+    })
+
     const submitHandler = useCallback(() => {
+        if (!formState.formIsValid) {
+            Alert.alert('Wrong input!', 'Please check the errors in the form.', [{ text: 'OK' }])
+            return
+        }
         if (editedProduct) {
-            dispatch(ProductActions.updateProduct(prodId, title, description, imageUrl))
+            dispatch(ProductActions.updateProduct(prodId, formState.inputValues.title, formState.inputValues.description, formState.inputValues.imageUrl))
         } else {
-            dispatch(ProductActions.createProduct(title, description, imageUrl, +price))
+            dispatch(ProductActions.createProduct(formState.inputValues.title, formState.inputValues.description, formState.inputValues.imageUrl, +formState.inputValues.price))
         }
         props.navigation.goBack()
-    }, [dispatch, prodId, title, description, imageUrl, price])
+    }, [dispatch, prodId, formState])
 
     useEffect(() => {
         props.navigation.setParams({ submit: submitHandler })
     }, [submitHandler])
+
+    const textChangeHandler = (inputIdentifier, text) => {
+        let isValid = false
+        if (text.trim().length > 0) {
+            isValid = true
+        }
+        dispatchFormState({ type: FORM_INPUT_UPDATE, value: text, isValid: isValid, input: inputIdentifier })
+    }
 
     return (
         <ScrollView>
             <View style={styles.form}>
                 <View style={styles.formControl}>
                     <Text style={styles.label}>Title</Text>
-                    <TextInput style={styles.input} value={title} onChangeText={text => setTitle(text)} />
+                    <TextInput style={styles.input} value={formState.inputValues.title} onChangeText={textChangeHandler.bind(this, 'title')}
+                        keyboardType='default' autoCapitalize='sentences' autoCorrect returnKeyType='next'
+                        onEndEditing={() => { }}
+                        onSubmitEditing={() => { }} />
+                    {!formState.inputValidities.title && <Text>Please enter a valid title</Text>}
                 </View>
-
                 <View style={styles.formControl}>
                     <Text style={styles.label}>Image URL</Text>
-                    <TextInput style={styles.input} value={imageUrl} onChangeText={text => setImageUrl(text)} />
+                    <TextInput style={styles.input} value={formState.inputValues.imageUrl} onChangeText={textChangeHandler.bind(this, 'imageUrl')} />
                 </View>
 
                 {editedProduct ? null : <View style={styles.formControl}>
                     <Text style={styles.label}>Price</Text>
-                    <TextInput style={styles.input} value={price} onChangeText={text => setPrice(text)} />
+                    <TextInput style={styles.input} value={formState.inputValues.price} onChangeText={textChangeHandler.bind(this, 'price')} keyboardType='decimal-pad' />
                 </View>}
 
                 <View style={styles.formControl}>
                     <Text style={styles.label}>Description</Text>
-                    <TextInput style={styles.input} value={description} onChangeText={text => setDescription(text)} />
+                    <TextInput style={styles.input} value={formState.inputValues.description} onChangeText={textChangeHandler.bind(this, 'description')} />
                 </View>
             </View>
         </ScrollView>
