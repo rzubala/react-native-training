b406e45270ed6f25221fb9a034f705a2b34744b9
Rafał Zubala <rzubala@gmail.com>
2020-02-24 09:40:24 +0100

    redux thunk

--- practice-shop/App.js
+++ practice-shop/App.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import { AppLoading } from 'expo'
 import * as Font from 'expo-font'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import ReduxThunk from 'redux-thunk'
 
 import ShopNavigator from './navigation/ShopNavigator'
 
@@ -11,13 +12,17 @@ import productsReducer from './store/reducers/products'
 import cartReducer from './store/reducers/cart'
 import ordersReducer from './store/reducers/orders'
 
+//https://practise-shop.firebaseio.com/
+//practise-shop: 
+
 const rootReducer = combineReducers({
   products: productsReducer,
   cart: cartReducer,
   orders: ordersReducer
 })
 
-const store = createStore(rootReducer, composeWithDevTools())
+//const store = createStore(rootReducer, composeWithDevTools())
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk))
 
 const fetchFonts = () => {
   return Font.loadAsync({
