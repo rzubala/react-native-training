b28355ce5b58192f643cebc389f5a49a09f7a207
Rafał Zubala <rzubala@gmail.com>
2020-03-01 20:31:46 +0100

    redux

--- native/screens/NewPlaceScreen.js
+++ native/screens/NewPlaceScreen.js
@@ -1,26 +1,76 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import Colors from '../constants/Colors'
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  ScrollView,
+  View,
+  Text,
+  Button,
+  TextInput,
+  StyleSheet
+} from "react-native";
+
+import * as placeActions from "../store/actions/places";
+import Colors from "../constants/Colors";
 
 const NewPlaceScreen = props => {
-    return (
-    <View style={styles.screen}>
-        <Text>NewPlaceScreen</Text>
-    </View>)
-}
+  const [titleValue, setTitleValue] = useState("");
+
+  const dispatch = useDispatch();
+
+  const titleChangeHandler = text => {
+    setTitleValue(text);
+  };
+
+  const savePlaceHandler = () => {
+    dispatch(placeActions.addPlace(titleValue));
+    props.navigation.goBack();
+  };
+
+  return (
+    <ScrollView>
+      <View style={styles.form}>
+        <Text style={styles.label}>Title</Text>
+        <TextInput
+          style={styles.textInput}
+          value={titleValue}
+          onChangeText={titleChangeHandler}
+        />
+        <Button
+          title="Save Place"
+          color={Colors.primary}
+          onPress={savePlaceHandler}
+        />
+      </View>
+    </ScrollView>
+  );
+};
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-})
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  form: {
+    margin: 30
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 15
+  },
+  textInput: {
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+    marginBottom: 15,
+    paddingVertical: 4,
+    paddingHorizontal: 2
+  }
+});
 
 NewPlaceScreen.navigationOptions = navData => {
-    return {
-      headerTitle: "Add new place",
-    }
-}
+  return {
+    headerTitle: "Add new place"
+  };
+};
 
-export default NewPlaceScreen
+export default NewPlaceScreen;
